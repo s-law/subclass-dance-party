3f1f233e69c1ff711c0c59d58c9a98a85ce6225b
s-law <slaw@nuclearanvil.com>
2015-11-30 15:51:38 -0800

    Bound correct frame of reference to step function.

--- src/dancer.js
+++ src/dancer.js
@@ -3,6 +3,8 @@ var Dancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
@@ -14,7 +16,10 @@ Dancer.prototype.constructor = Dancer;
 Dancer.prototype.step = function() {
   // the basic Dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+
+  setTimeout(function() {
+    this.step();
+  }.bind(this), this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left) {
