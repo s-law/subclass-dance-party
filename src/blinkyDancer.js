45cdeea6f88092ee915585570f50fb52fcc3a581
s-law <slaw@nuclearanvil.com>
2015-11-30 15:50:31 -0800

    Changed call to explicitly reference superclass's step.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -7,7 +7,8 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  this.step();
+  
+  Dancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
