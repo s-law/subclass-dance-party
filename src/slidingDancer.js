515994a1a9a8deedc4fd627d2c212809fea0cbb6
s-law <slaw@nuclearanvil.com>
2015-11-30 20:17:43 -0800

    Randomly generate a speed for new instance.

--- src/slidingDancer.js
+++ src/slidingDancer.js
@@ -1,15 +1,14 @@
 var SlidingDancer = function(top, left, timeBetweenSteps) {
-  this.ySpeed = 25;
-  this.xSpeed = 25;
   Dancer.call(this, top, left, 25);
+  this.ySpeed = 10 + (10 * Math.random());
+  this.xSpeed = 10 + (10 * Math.random());
 };
 
 SlidingDancer.prototype = Object.create(Dancer.prototype);
 SlidingDancer.prototype.constructor = SlidingDancer;
 
 SlidingDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
-
+  // call the old version of step at the beginning of any call to this new version of setPosition
   Dancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
@@ -18,11 +17,11 @@ SlidingDancer.prototype.step = function() {
   var width = $("body").width();
   var height = $("body").height();
 
-  if (this.left >= width || this.left <= 0) {
+  if (this.left >= width - this.img.width || this.left <= 0) {
     this.xSpeed *= -1;
   }
 
-  if (this.top >= height || this.top <= 0) {
+  if (this.top >= height - this.img.height || this.top <= 0) {
     this.ySpeed *= -1;
   }
 
