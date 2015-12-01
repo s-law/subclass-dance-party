6380e6be9b794f2525ee846d99818ff557a7c10b
s-law <slaw@nuclearanvil.com>
2015-12-01 13:03:21 -0800

    Added new event handlers.

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   window.dancers = [];
+  window.players = [];
 
   $(".addDancerButton").on("click", function(event) {
     /* This function sets up the click handlers for the create-dancer
@@ -28,25 +29,71 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
-    window.dancers.push(dancer);
+    if (dancer.role === "player") {
+      window.players.push(dancer);
+    } else {
+      window.dancers.push(dancer);
+    }
   });
 
   $(".lineUpButton").on("click", function(event) {
-    var gap = $("body").width() / (window.dancers.length);
+    var gap = $("body").width() / (window.players.length);
     var mark = 20;
-    for (var i = 0; i < window.dancers.length; i++) {
-      var node = window.dancers[i];
-      node.lineUp(mark);
+    for (var i = 0; i < window.players.length; i++) {
+      var node = window.players[i];
+      var last = window.players.length - 1 === i ? true : false;
+      node.lineUp(mark, last);
       mark += gap;
+    }
+    if ($(".lineUpButton").text() === "come back") {
+      $(".lineUpButton").text("line up");
+      $(".skateButton").show();
+      $(".leaveButton").show();
     }
   });
 
   $(".skateButton").on("click", function(event) {
-    for (var i = 0; i < window.dancers.length; i++) {
-      var node = window.dancers[i];
+    for (var i = 0; i < window.players.length; i++) {
+      var node = window.players[i];
       node.linedUp = false;
       node.step();
     }
+  });
+
+  $(".leaveButton").on("click", function(event) {
+    for (var i = 0; i < window.players.length; i++) {
+      var node = window.players[i];
+      node.leaveIce();
+    }
+    $(".skateButton").hide();
+    $(".leaveButton").hide();
+    $(".lineUpButton").text("come back");
+  });
+  
+  $(".zamboniButton").on("click", function(event) {
+    $(".leaveButton").click();
+
+    var snoopy = new Zamboni(0);
+
+    snoopy.clean();
+
+    // for (var i = 0; i < $('body').height()/snoopy.imag; i++) {
+      // snoopy.$node.animate({ 
+      // top: snoopy.top,
+      // left: $("body").width() + snoopy.img.width
+      // }, 2000 );
+
+    //   snoopy.top = snoopy.top + snoopy.img.height;
+    //   snoopy.left = $("body").width() + snoopy.img.width;
+      
+    //   snoopy.$node.animate({ 
+    //   top: snoopy.top,
+    //   left: -1 * snoopy.img.width
+    //   }, 2000 );
+
+    //   snoopy.top = snoopy.top + snoopy.img.height;
+    //   snoopy.left = -1 * snoopy.img.width;      
+    // }
   });
 });
 
