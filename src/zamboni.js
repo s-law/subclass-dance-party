b68b1aaef362a67bbff169506c3e54b42aab7143
s-law <slaw@nuclearanvil.com>
2015-12-01 18:53:15 -0800

    Fixed bounding problem.

--- src/zamboni.js
+++ src/zamboni.js
@@ -1,12 +1,11 @@
 var Zamboni = function(top, left, timeBetweenSteps) {
   var image = new Image();
   image.src = 'images/zamboni.jpg';
+  this.img = image;
 
   Dancer.call(this, top, 5 * image.height, 25);
   this.role = "Zamboni";
   this.$node = $('<span class="dancer"><img src="images/zamboni.gif" /></span>');
-
-  this.img = image;
 };
 
 Zamboni.prototype = Object.create(Dancer.prototype);
@@ -16,19 +15,20 @@ Zamboni.prototype.clean = function() {
   this.left = -1 * this.img.width;
   this.setPosition();
 
-  $('body').append(this.$node)
+  $('body').append(this.$node);
 
   var pass = function(dest) {
+    var move = dest + $('body').width()/2;
     this.$node.toggleClass('flipped');
     this.$node.animate({ 
     top: this.top,
-    left: dest
+    left: move
     }, 2000, function() {
       this.top += this.img.height;
-      this.left = dest;
+      this.left = move;
       this.setPosition();
-      if (this.top < $('body').height()) {
-        pass(-1*dest);
+      if (this.top + this.img.height < $('body').height()) {
+        pass(-1*(dest+this.img.width));
       } else {
         window.wasZamboniCalled = true;
         $(".lineUpButton").click();
@@ -37,5 +37,5 @@ Zamboni.prototype.clean = function() {
     }.bind(this));
   }.bind(this);
 
-  pass($('body').width() + 100);
+  pass($('body').width()/2 + this.img.width);
 };
